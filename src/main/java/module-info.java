a869f504e875a2fc86ab7d834ed01be737386a15
New Media Works <oss@newmediaworks.com>
2022-04-27 00:08:12 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -30,4 +30,4 @@ module com.newmediaworks.oss.website {
   requires com.semanticcms.core.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-all</artifactId>
   requires com.semanticcms.news.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-news-all</artifactId>
   requires com.semanticcms.section.all; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-section-all</artifactId>
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
